ae25bd6f8672152c93a63b1d5fed917185bde50d
letrananhvu <letrananhvu>
2020-09-22 02:06:20 +0300

    establish socket io

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Routes from './Routes'
+import socket from './services/socket'
 
 export default function App() {
+  const emit = async () => {
+    socket.send('hahaha')
+  }
+  useEffect(() => {}, [])
+
   return (
     <>
+      <button onClick={emit}>send</button>
       <Routes />
     </>
   )
---
+++ src/services/socket.ts
@@ -0,0 +1,8 @@
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:5000')
+
+socket.on('connect', () => {
+  console.log('connected to socket')
+})
+export default socket
